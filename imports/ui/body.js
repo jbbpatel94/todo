846016d0cfafa2f6ad209779ed7589f8a241205a
jbbpatel94 <jbbpatel94@gmail.com>
2017-07-19 17:51:22 +0530

    Storing temporary UI state in a Reactive Dictionary

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,15 +1,26 @@
 import { Template } from 'meteor/templating';
-
+import { ReactiveDict } from 'meteor/reactive-dict';
 import { Tasks } from '../api/tasks.js';
- 
+
 import './task.js';
 import './body.html';
 
+Template.body.onCreated(function bodyOnCreated() {
+	this.state = new ReactiveDict();
+});
 Template.body.helpers({
-  tasks() {
-    // Show newest tasks at the top by sorting
+	tasks() {
+		const instance = Template.instance();
+		if (instance.state.get('hideCompleted')) {
+      // If hide completed is checked, filter tasks
+      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+  }
+    // Otherwise, return all of the tasks
     return Tasks.find({}, { sort: { createdAt: -1 } });
-  },
+},
+incompleteCount() {
+	return Tasks.find({ checked: { $ne: true } }).count();
+}
 });
 Template.body.events({
 	'submit form':function(event){
@@ -18,7 +29,10 @@ Template.body.events({
 		const text = target.text.value;
 		createdAt = new Date();
 	//insert into collection named Tasks
-		Tasks.insert({text :text ,createdAt:createdAt});
-		target.text.value = '';
-	}
+	Tasks.insert({text :text ,createdAt:createdAt});
+	target.text.value = '';
+},
+'change .hide-completed input'(event, instance) {
+	instance.state.set('hideCompleted', event.target.checked);
+}
 });
